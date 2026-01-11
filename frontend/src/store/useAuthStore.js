edffd3a506d4847764ff63fdb59fc54dfb963bba
VaibhavRajDwivedi <vaibhavrajdwivedi65@gmail.com>
2026-01-11 13:31:47 +0530

    fix socket urls

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const BASE_URL = import.meta.env.MODE === 'development'
     ? 'http://localhost:3000'
-    : '/'
+    : 'https://flash-chat-7y8k.onrender.com'
 
 export const useAuthStore = create((set,get) => ({
   authUser: null,
