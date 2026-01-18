914462c63fa37a0200ce044626761704887e74d1
VaibhavRajDwivedi <vaibhavrajdwivedi65@gmail.com>
2026-01-19 01:24:38 +0530

    fix : video call video error

--- frontend/src/components/VideoCall.jsx
+++ frontend/src/components/VideoCall.jsx
@@ -29,7 +29,14 @@ const VideoCall = () => {
     const [isConnected, setIsConnected] = useState(false);
 
     const rtcConfig = {
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+        iceServers: [
+            {
+                urls: [
+                    "stun:stun.l.google.com:19302",
+                    "stun:global.stun.twilio.com:3478",
+                ],
+            },
+        ],
     };
 
     useEffect(() => {
